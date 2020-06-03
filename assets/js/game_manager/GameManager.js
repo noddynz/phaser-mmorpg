e064c358d54511c0c221ad120ccbd944828d7255
Michael Morris <michael@turkspoultry.co.nz>
2020-06-04 10:50:48 +1200

    save

--- assets/js/game_manager/GameManager.js
+++ assets/js/game_manager/GameManager.js
@@ -1,55 +1,80 @@
+/*jshint esversion: 6 */
 class GameManager {
-	constructor(scene, mapData) {
-		this.scene = scene;
-		this.mapData = mapData;
+  constructor(scene, mapData) {
+    this.scene = scene;
+    this.mapData = mapData;
 
-		this.spawners = {};
-		this.chests = {};
+    this.spawners = {};
+    this.chests = {};
 
-		this.playerLocations = [];
-		this.chestLocations = {};
-		this.monsterLocations = {};
-	}
+    this.playerLocations = [];
+    this.chestLocations = {};
+    this.monsterLocations = {};
+  }
 
-	setup() {
-		this.parseMapData();
-		this.setupEventListener();
-		this.setupSpawners();
-		this.spawnPlayer();
-	}
+  setup() {
+    this.parseMapData();
+    this.setupEventListener();
+    this.setupSpawners();
+    this.spawnPlayer();
+  }
 
-	parseMapData() {
-		this.mapData.forEach((layer) => {
-			if (layer.name === 'player_locations') {
-				layer.objects.forEach((obj) => {
-					this.playerLocations.push([ obj.x, obj.y ]);
-				});
-			} else if (layer.name === 'chest_locations') {
-				layer.objects.forEach((obj) => {
-					if (this.chestLocations[obj.properties.spawner]) {
-						this.chestLocations[obj.properties.spawner].push([ obj.x, obj.y ]);
-					} else {
-						this.chestLocations[obj.properties.spawner] = [ [ obj.x, obj.y ] ];
-					}
-				});
-			} else if (layer.name === 'monster_locations') {
-				layer.objects.forEach((obj) => {
-					if (this.monsterLocations[obj.properties.spawner]) {
-						this.monsterLocations[obj.properties.spawner].push([ obj.x, obj.y ]);
-					} else {
-						this.monsterLocations[obj.properties.spawner] = [ [ obj.x, obj.y ] ];
-					}
-				});
-			}
-		});
-	}
+  parseMapData() {
+    this.mapData.forEach((layer) => {
+      if (layer.name === 'player_locations') {
+        layer.objects.forEach((obj) => {
+          this.playerLocations.push([obj.x, obj.y]);
+        });
+      } else if (layer.name === 'chest_locations') {
+        layer.objects.forEach((obj) => {
+          if (this.chestLocations[obj.properties.spawner]) {
+            this.chestLocations[obj.properties.spawner].push([obj.x, obj.y]);
+          } else {
+            this.chestLocations[obj.properties.spawner] = [[obj.x, obj.y]];
+          }
+        });
+      } else if (layer.name === 'monster_locations') {
+        layer.objects.forEach((obj) => {
+          if (this.monsterLocations[obj.properties.spawner]) {
+            this.monsterLocations[obj.properties.spawner].push([obj.x, obj.y]);
+          } else {
+            this.monsterLocations[obj.properties.spawner] = [[obj.x, obj.y]];
+          }
+        });
+      }
+    });
+  }
 
-	setupEventListener() {}
+  setupEventListener() {}
 
-	setupSpawners() {}
+  setupSpawners() {
+    // create chest spawners
+    Object.keys(this.chestLocations).forEach((key) => {
+      const config = {
+        spawnInterval: 3000,
+        limit: 3,
+        spawnerType: 'CHEST',
+        id: `chest-${key}`,
+      };
 
-	spawnPlayer() {
-		const location = this.playerLocations[Math.floor(Math.random() * this.playerLocations.length)];
-		this.scene.events.emit('spawnPlayer', location);
-	}
+      const spawner = new Spawner(
+        config,
+        this.chestLocations[key],
+        this.addChest.bind(this),
+        this.deleteChest.bind(this)
+      );
+      this.spawners[spawner.id] = spawner;
+    });
+  }
+
+  spawnPlayer() {
+    const location = this.playerLocations[
+      Math.floor(Math.random() * this.playerLocations.length)
+    ];
+    this.scene.events.emit('spawnPlayer', location);
+  }
+
+  addChest() {}
+
+  deleteChest() {}
 }
--- assets/js/game_manager/Spawner.js
+++ assets/js/game_manager/Spawner.js
@@ -1,3 +1,34 @@
+/*jshint esversion: 6 */
 class Spawner {
-	constructor() {}
+  constructor(config, spawnLocations, addObject, deleteObject) {
+    this.id = config.id;
+    this.spawnInterval = config.spawnInterval;
+    this.limit = config.limit;
+    this.objectType = config.spawnerType;
+    this.spawnLocations = spawnLocations;
+    this.addObject = addObject;
+    this.deleteObject = deleteObject;
+
+    this.objectsCreated = [];
+
+    this.start();
+  }
+
+  start() {
+    this.interval = setInterval(() => {
+      if (this.objectsCreated.length < this.limit) {
+        this.spawnObject();
+      }
+    }, this.spawnInterval);
+  }
+
+  spawnObject() {
+    console.log('spawning object');
+  }
+
+  spawnChest() {}
+
+  pickRandomLocation() {}
+
+  removeObject() {}
 }
